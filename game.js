43ee331c58017eef9e30be0cfe77349292e73a7c
imstupid927 <tosavitarora@gmail.com>
2025-02-15 15:01:45 -0800

    Add files via upload

--- game.js
+++ game.js
@@ -6,6 +6,7 @@ const CANVAS_HEIGHT = 600;
 canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
 
+let score = 0;
 let gameObjects = [];
 
 class Block {
@@ -15,17 +16,39 @@ class Block {
         this.width = width;
         this.height = height;
         this.color = color;
+        this.isDestroyed = false;
     }
 
     draw() {
-        ctx.fillStyle = this.color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        if (!this.isDestroyed) {
+            ctx.fillStyle = this.color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
+    }
+
+    checkCollision(mouseX, mouseY) {
+        if (
+            mouseX >= this.x &&
+            mouseX <= this.x + this.width &&
+            mouseY >= this.y &&
+            mouseY <= this.y + this.height
+        ) {
+            this.isDestroyed = true;
+            score++;
+            document.getElementById('score').innerText = `Score: ${score}`;
+        }
     }
 }
 
 function initializeGame() {
-    gameObjects.push(new Block(50, 50, 100, 100, 'red'));
-    gameObjects.push(new Block(200, 200, 100, 100, 'blue'));
+    for (let i = 0; i < 10; i++) {
+        let x = Math.random() * (CANVAS_WIDTH - 50);
+        let y = Math.random() * (CANVAS_HEIGHT - 50);
+        let width = 50;
+        let height = 50;
+        let color = `hsl(${Math.random() * 360}, 100%, 50%)`;
+        gameObjects.push(new Block(x, y, width, height, color));
+    }
 }
 
 function update() {
@@ -33,6 +56,14 @@ function update() {
     gameObjects.forEach(obj => obj.draw());
     requestAnimationFrame(update);
 }
+
+canvas.addEventListener('click', (event) => {
+    const rect = canvas.getBoundingClientRect();
+    const mouseX = event.clientX - rect.left;
+    const mouseY = event.clientY - rect.top;
+
+    gameObjects.forEach(obj => obj.checkCollision(mouseX, mouseY));
+});
 
 initializeGame();
 update();
